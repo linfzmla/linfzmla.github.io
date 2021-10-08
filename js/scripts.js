5f235e6c36e329293533f6954a5291b4ba681d82
linfzmla <linfzmla@gmail.com>
2021-10-08 12:12:15 -0500

    tercera actualizacion

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,17 @@ $(document).ready(function () {
 
 
 /*----------------------------------------*/
+
+(function() {
+    //a partir de que punto del scroll vertical de la ventana mostrará el botón
+    const ishow = 115
+    const $divtop = document.getElementById("div-totop")
+    window.addEventListener("scroll", function() {
+        if(document.documentElement.scrollTop > ishow){
+            $divtop.style.display = "inherit"
+        }
+        else {
+            $divtop.style.display = "none"
+        }
+    })
+})()
