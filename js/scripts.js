de320c6cc36a09141136b102f7cfc9f2703f762e
linfzmla <linfzmla@gmail.com>
2021-11-12 11:16:32 -0600

    actualizacion

--- js/scripts.js
+++ js/scripts.js
@@ -16,16 +16,3 @@ $(document).ready(function () {
 
 /*----------------------------------------*/
 
-(function() {
-    //a partir de que punto del scroll vertical de la ventana mostrará el botón
-    const ishow = 115
-    const $divtop = document.getElementById("div-totop")
-    window.addEventListener("scroll", function() {
-        if(document.documentElement.scrollTop > ishow){
-            $divtop.style.display = "inherit"
-        }
-        else {
-            $divtop.style.display = "none"
-        }
-    })
-})()
